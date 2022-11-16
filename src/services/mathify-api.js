585aeecb03430df4015ccb5b4102f55847ef9af7
danielpqb <danielpqb@gmail.com>
2022-11-16 18:21:04 -0300

    dotenv deploy fix

--- src/services/mathify-api.js
+++ src/services/mathify-api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = process.env.API_BASE_URL;
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+console.log(BASE_URL);
 
 export default function getUserDataByToken(token) {
   return axios.get(`${BASE_URL}/users/me`, {
