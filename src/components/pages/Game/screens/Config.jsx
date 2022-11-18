7982b85f0dac80af413227e59b0d3bb9b3e4b353
danielpqb <danielpqb@gmail.com>
2022-11-18 19:25:43 -0300

    Start button on config screen

--- src/components/pages/Game/screens/Config.jsx
+++ src/components/pages/Game/screens/Config.jsx
@@ -7,15 +7,35 @@ export default function Config() {
   const { setGameData } = useContext(AppContext);
 
   return (
-    <Container
-      onClick={() => {
-        changeScreen(setGameData, "question");
-      }}
-    ></Container>
+    <Container>
+      <StartButton
+        onClick={() => {
+          changeScreen(setGameData, "question");
+        }}
+      >
+        Start
+      </StartButton>
+    </Container>
   );
 }
 
 const Container = styled.div`
   & {
+  }
+`;
+
+const StartButton = styled.div`
+  & {
+    background-color: #213a7e;
+
+    border-radius: 10px;
+
+    border: 4px solid rgba(0, 0, 0, 0.4);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+    max-width: 150px;
+    height: 60px;
+
+    justify-self: center;
   }
 `;
