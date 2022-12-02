e9730033fdaf6f3c0391b16ee7e76f4b70ab1bad
danielpqb <danielpqb@gmail.com>
2022-12-02 19:53:18 -0300

    Enhancing console.log() visualization

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,11 +25,16 @@ export default function App() {
     const headerColor = "color: #ffbe88; font-weight: bold;";
     const paramColor = "\x1B[38;2;126;204;152m"; //rgb(126, 204, 152)"
     const dependencyColor = "\x1B[38;2;255;154;136m"; //rgb(255, 154, 136)"
+    const stringColor = "\x1B[38;2;54;206;193m"; //rgb(54, 206, 193)"
     const resetColor = "\x1B[m";
 
     const separateParams = (obj) => {
       if (obj) {
         Object.entries(obj).forEach((keyValuePair) => {
+          if (typeof keyValuePair[1] === "string") {
+            console.log(`  ${paramColor}${keyValuePair[0]}: ${stringColor}'${keyValuePair[1]}'`);
+            return;
+          }
           console.log(`  ${paramColor}${keyValuePair[0]}:`, keyValuePair[1]);
         });
       }
