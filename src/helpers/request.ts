88d1c8a220e913f5194c4f659d96444b1acd27ea
danielpqb <danielpqb@gmail.com>
2023-01-20 14:27:11 -0300

    Fix axios typing

--- src/helpers/request.ts
+++ src/helpers/request.ts
@@ -1,21 +1,37 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+import axios, {
+  AxiosError,
+  AxiosRequestConfig,
+  AxiosResponse,
+  RawAxiosRequestConfig,
+} from "axios";
 
 export type RequestPromise = Promise<AxiosResponse<any, any>>;
 export type Response = AxiosResponse;
 export type Error = AxiosError;
 
-export async function get(url: string, config?: AxiosRequestConfig<any> | undefined) {
+export async function get(
+  url: string,
+  config?: RawAxiosRequestConfig<any> | undefined
+) {
   const promise = axios.get(url, config);
   return promise as RequestPromise;
 }
 
-export async function post(url: string, data: any, config?: AxiosRequestConfig<any> | undefined) {
+export async function post(
+  url: string,
+  data: any,
+  config?: AxiosRequestConfig<any> | undefined
+) {
   const promise = axios.post(url, data, config);
   return promise as RequestPromise;
 }
 
-export async function put(url: string, data: any, config?: AxiosRequestConfig<any> | undefined) {
+export async function put(
+  url: string,
+  data: any,
+  config?: AxiosRequestConfig<any> | undefined
+) {
   const promise = axios.put(url, data, config);
   return promise as RequestPromise;
 }
--- src/services/user-services.ts
+++ src/services/user-services.ts
@@ -13,10 +13,18 @@ export function postSignIn(body: { email: string; password: string }) {
   return post(`${BASE_URL}/auth`, body);
 }
 
-export function postSignUp(body: { name: string; email: string; password: string }) {
+export function postSignUp(body: {
+  name: string;
+  email: string;
+  password: string;
+}) {
   return post(`${BASE_URL}/users/sign-up`, body);
 }
 
-export function putOAuth(body: { name: string; email: string; photoUrl: string }): Promise<AxiosResponse<{token: string}, any>> {
+export function putOAuth(body: {
+  name: string;
+  email: string;
+  photoUrl: string;
+}): Promise<AxiosResponse<{ token: string }, any>> {
   return put(`${BASE_URL}/users/oauth`, body);
 }
